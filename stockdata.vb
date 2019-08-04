48a58d1892c3eaaca47d2260017a35bfb9bf2d30
Mae <lisaduncan81@gmail.com>
2019-08-03 22:01:56 -0500

    updated easy

--- stockdata.vb
+++ stockdata.vb
@@ -1,40 +1,44 @@
-Sub StockData()
-    ' Set Dimensions
-    Dim total As Double
-
-    ' get the row number of the last row with data
-    RowCount = Cells(Rows.Count, "A").End(xlUp).Row
-
-    ' Set title row
-    Range("I1").Value = "Ticker"
-    Range("J1").Value = "Total Stock Volume"
-
-    For i = 2 To RowCount
-
-        ' If ticker changes then print results
-        If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
-
-            ' Stores results in variable
-            total = total + Cells(i, 7).Value
-
-            ' Print ticker symbol
-            Range("I" & 2 + j).Value = Cells(i, 1).Value
-
-            ' Print total
-            Range("J" & 2 + j).Value = total
-
-            ' Reset Total
-            total = 0
-
-            ' Move to next row
-            j = j + 1
-
-        ' Else keep adding to the total volume
-        Else
-            total = total + Cells(i, 7).Value
-
-        End If
-
-    Next i
+Sub Worksheet()
+    Dim ws As Worksheet
+    Application.ScreenUpdating = False
+    For Each ws In Worksheets
+        ws.Select
+        Call Stock_year
+    Next ws
+    Application.ScreenUpdating = True
 End Sub
-••••ˇˇˇˇ
+
+Sub Stock_year()
+
+Dim Ticker As String
+Dim Total_Stock_Volume As Double
+Dim Summary_Table_Index As Double
+
+
+Range("I1").Value = "Ticker"
+Range("J1").Value = "Total_Stock_Volume"
+Summary_Table_Index = 2
+
+For i = 2 To 797711
+
+
+    If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
+        Ticker = Cells(i, 1).Value
+        Total_Stock_Volume = Total_Stock_Volume + Cells(i, 7).Value
+
+        Range("I" & 1 + Summary_Table_Index).Value = Ticker
+        Range("J" & 1 + Summary_Table_Index).Value = Total_Stock_Volume
+                
+        Summary_Table_Index = Summary_Table_Index + 1
+
+        Total_Stock_Volume = 0
+
+    Else
+        Total_Stock_Volume = Total_Stock_Volume + Cells(i, 7).Value
+    End If
+
+
+        
+Next i
+
+End Sub
